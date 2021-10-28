f8dc6b33fde52a448329764607402c79d8bdb191
taibislamdipu <mailtaibislam@gmail.com>
2021-10-28 15:48:48 +0600

    added port

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
 const cors = require('cors');
 require('dotenv').config()
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 // middleware
@@ -24,7 +26,25 @@ async function run() {
         const database = client.db('carMechanic')
         const servicesCollection = database.collection("services");
 
-        // post api
+        // GET API
+        app.get('/services', async (req, res) => {
+
+            const cursor = servicesCollection.find({})
+            const services = await cursor.toArray();
+            res.send(services);
+
+        })
+
+        // GET Single Service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('getting specific service', id);
+            const query = { _id: ObjectId(id) };
+            const service = await servicesCollection.findOne(query);
+            res.json(service);
+        })
+
+        // POST API
         app.post('/services', async (req, res) => {
             const service = req.body;
             console.log('hit the post api', service);
@@ -36,6 +56,21 @@ async function run() {
 
         })
 
+        // DELETE API
+        app.delete('/services/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: ObjectId(id) };
+            const result = await servicesCollection.deleteOne(query);
+            res.json(result);
+
+            if (result.deletedCount === 1) {
+                console.log("Successfully deleted one document.");
+            } else {
+                console.log("No documents matched the query. Deleted 0 documents.");
+            }
+        })
+
     }
     finally {
         // await client.close();
@@ -45,7 +80,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Running Genius Server')
+    res.send('Running Genius Car Mechanics Server')
 })
 
 app.listen(port, () => {
